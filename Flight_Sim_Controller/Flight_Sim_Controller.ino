df9f732904c27c17c35a7ed941e6be009386a700
Thomas TL <thomas.tarpin@gmail.com>
2021-06-05 16:59:32 +0800

    Added 1 joystick button

--- Flight_Sim_Controller/Flight_Sim_Controller.ino
+++ Flight_Sim_Controller/Flight_Sim_Controller.ino
@@ -4,10 +4,11 @@
 #define ROLL_PIN    A3
 #define PITCH_PIN   A2
 #define YAW_PIN     A1
+#define BUTTON_1    9
 
 Joystick_ Joystick (  JOYSTICK_DEFAULT_REPORT_ID,
                       JOYSTICK_TYPE_JOYSTICK,
-                      0,     // Number of available buttons
+                      1,     // Number of available buttons
                       0,      // Number of available hat switches
                       true,   // X Axis
                       true,   // Y Axis
@@ -21,18 +22,19 @@ Joystick_ Joystick (  JOYSTICK_DEFAULT_REPORT_ID,
                       false,  // Brake
                       false   // Steering
                      ); 
-
-//Joystick_ Joystick;
  
 int JoystickX;
 int JoystickY;
 int JoystickZ;
+
+int lastButton1State = 0;
 
 void setup() {
 
   pinMode(ROLL_PIN, INPUT_PULLUP);
   pinMode(PITCH_PIN, INPUT_PULLUP); 
   pinMode(YAW_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_1, INPUT_PULLUP);  
   
   Joystick.setXAxisRange(0, 1023); 
   Joystick.setYAxisRange(0, 1023);
@@ -43,17 +45,26 @@ void setup() {
 
 void loop() {
   
-// Read Joystick
+  // X axis is used for the plane roll
   JoystickX = analogRead(ROLL_PIN); // Hall effect sensor connects to this analog pin
-  JoystickY = analogRead(PITCH_PIN); // Hall effect sensor connects to this analog pin
-
-// Read Rudder Pedals
-  JoystickZ = analogRead(YAW_PIN); // Hall effect sensor connects to this analog pin
-   
-// Output Controls
   Joystick.setXAxis(JoystickX);
+
+  // Y axis is used for the plane pitch
+  JoystickY = analogRead(PITCH_PIN); // Hall effect sensor connects to this analog pin
   Joystick.setYAxis(JoystickY);
+  
+  // Z axis is used for rudder pedals
+  JoystickZ = analogRead(YAW_PIN); // Hall effect sensor connects to this analog pin
   Joystick.setZAxis(JoystickZ);
+   
+  // Read buttons state
+  int currentButton1State = !digitalRead(BUTTON_1);
+  if(currentButton1State != lastButton1State) {
+    Joystick.setButton(0, currentButton1State);
+    lastButton1State = currentButton1State;
+    // Pole delay/debounce
+    delay(10);    
+  }  
 
   Joystick.sendState();
 } 
